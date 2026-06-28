c4652f1191f4f89e0bd27af0def77360e21a87d8
PaniShews <kpetruh@gmail.com>
2026-06-28 15:14:27 +0300

    Solution

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Request, status
+from fastapi.exception_handlers import request_validation_exception_handler
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
@@ -13,10 +14,16 @@ app = FastAPI(
 
 @app.exception_handler(RequestValidationError)
 async def validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
-    return JSONResponse(
-        status_code=status.HTTP_400_BAD_REQUEST,
-        content={"detail": "Invalid input data."},
-    )
+    errors = exc.errors()
+    body_errors_only = errors and all(error["loc"][0] == "body" for error in errors)
+
+    if body_errors_only:
+        return JSONResponse(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            content={"detail": "Invalid input data."},
+        )
+
+    return await request_validation_exception_handler(request, exc)
 
 
 api_version_prefix = "/api/v1"
